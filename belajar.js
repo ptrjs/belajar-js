52ba9dbe0f5e2f0a87bb5e30f7b24bd7ec40c050
ptrjs <peterjose64@gmail.com>
2023-09-19 00:43:19 +0700

    upload

---
+++ belajar.js
@@ -0,0 +1,26 @@
+let angka1 = 20
+let angka2 = 30
+
+let tambah =angka1+angka2
+let kurang = angka1-angka2
+let kali = angka1*angka2
+let bagi = angka1/angka2
+let modulo = angka1%angka2
+
+console.log(tambah)
+console.log(kurang)
+console.log(kali)
+console.log(bagi)
+console.log(modulo)
+
+console.log("--------------")
+
+let firstName="asep"
+let lastName="minato"
+
+console.log(firstName+" "+lastName)
+
+// const ninja = "naruto"
+// console.log(ninja)
+// ninja = sasuke
+// console.log(ninja)
